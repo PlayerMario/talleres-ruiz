53b8402a5e2023c0b6fe4d088b261678e087b2ba
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2023-02-22 12:27:08 +0100

    Corrección vistas

--- flutter/talleresruiz/lib/widgets/mecanico/menu_lista_admec.dart
+++ flutter/talleresruiz/lib/widgets/mecanico/menu_lista_admec.dart
@@ -9,87 +9,108 @@ class MenuListaAdMec extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Column(children: [
-      Card(
-          margin:
-              const EdgeInsets.only(top: 45, left: 20, right: 20, bottom: 20),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(43, 45, 66, 1)),
-              child: const Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text(
-                    "CITAS",
-                    style: TextStyle(
-                        fontSize: 25, color: Color.fromRGBO(237, 242, 244, 1)),
-                    textAlign: TextAlign.start,
-                  )),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ProviderCitasListar(rol: rol);
-                }));
-              })),
-      Card(
-          margin:
-              const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(43, 45, 66, 1)),
-              child: const Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text(
-                    "CLIENTES",
-                    style: TextStyle(
-                        fontSize: 25, color: Color.fromRGBO(237, 242, 244, 1)),
-                    textAlign: TextAlign.start,
-                  )),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return const ProviderClientesListar();
-                }));
-              })),
-      Card(
-          margin:
-              const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(43, 45, 66, 1)),
-              child: const Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text(
-                    "MECÁNICOS",
-                    style: TextStyle(
-                        fontSize: 25, color: Color.fromRGBO(237, 242, 244, 1)),
-                    textAlign: TextAlign.start,
-                  )),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ProviderMecanicosListar(rol: rol);
-                }));
-              })),
-      Card(
-          margin:
-              const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(43, 45, 66, 1)),
-              child: const Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text(
-                    "TRABAJADORES",
-                    style: TextStyle(
-                        fontSize: 25, color: Color.fromRGBO(237, 242, 244, 1)),
-                    textAlign: TextAlign.start,
-                  )),
-              onPressed: () {
-                if (rol == "ADMIN") {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const AdMecMenuTrbj();
-                  }));
-                } else {
-                  showSnackbar("Sólo un ADMIN puede acceder", context);
-                }
-              })),
-    ])));
+            child: SingleChildScrollView(
+                child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: SizedBox(
+                        child: Column(children: [
+                      Card(
+                          margin: const EdgeInsets.only(
+                              top: 45, left: 20, right: 20, bottom: 20),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromRGBO(43, 45, 66, 1)),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(30),
+                                  child: Text(
+                                    "CITAS",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color:
+                                            Color.fromRGBO(237, 242, 244, 1)),
+                                    textAlign: TextAlign.start,
+                                  )),
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return ProviderCitasListar(rol: rol);
+                                }));
+                              })),
+                      Card(
+                          margin: const EdgeInsets.only(
+                              top: 30, left: 20, right: 20, bottom: 20),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromRGBO(43, 45, 66, 1)),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(30),
+                                  child: Text(
+                                    "CLIENTES",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color:
+                                            Color.fromRGBO(237, 242, 244, 1)),
+                                    textAlign: TextAlign.start,
+                                  )),
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const ProviderClientesListar();
+                                }));
+                              })),
+                      Card(
+                          margin: const EdgeInsets.only(
+                              top: 30, left: 20, right: 20, bottom: 20),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromRGBO(43, 45, 66, 1)),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(30),
+                                  child: Text(
+                                    "MECÁNICOS",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color:
+                                            Color.fromRGBO(237, 242, 244, 1)),
+                                    textAlign: TextAlign.start,
+                                  )),
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return ProviderMecanicosListar(rol: rol);
+                                }));
+                              })),
+                      Card(
+                          margin: const EdgeInsets.only(
+                              top: 30, left: 20, right: 20, bottom: 20),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromRGBO(43, 45, 66, 1)),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(30),
+                                  child: Text(
+                                    "TRABAJADORES",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color:
+                                            Color.fromRGBO(237, 242, 244, 1)),
+                                    textAlign: TextAlign.start,
+                                  )),
+                              onPressed: () {
+                                if (rol == "ADMIN") {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) {
+                                    return const AdMecMenuTrbj();
+                                  }));
+                                } else {
+                                  showSnackbar(
+                                      "Sólo un ADMIN puede acceder", context);
+                                }
+                              })),
+                    ]))))));
   }
 }
